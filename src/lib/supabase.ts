28d2611a4a62ec915040df6ccf87c622bc7acddd
boatclosers <Garry@SeaYachts.net>
2026-06-09 14:05:03 -0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://xoihnmkgncuocikmgs.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvaWhubWtnbmN1b2N4aWtudmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NTAwNzgsImV4cCI6MjA5NjUyNjA3OH0.NQBD6pAMHkz-PMtd7sKzOeiWUDfL4MnNJU6AThQAL64'
+)
